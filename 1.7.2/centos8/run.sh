9add7d8a4f1ba03b5ad85bcb1766fe55fdefe8dc
mtakahashiif <18286485+mtakahashiif@users.noreply.github.com>
2021-06-03 14:45:10 +0900

    remove 1.7.2

--- 1.7.2/centos8/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-EXASTRO_ITA_VER=1.7.2
-EXASTRO_ITA_LANG=${1:-ja}
-
-docker run --privileged --add-host=exastro-it-automation:127.0.0.1 -d -p 8080:80 -p 10443:443 it-automation:${EXASTRO_ITA_VER}-centos8-${EXASTRO_ITA_LANG}
