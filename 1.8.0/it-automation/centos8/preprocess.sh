ce7f64e244b0859740b00e4a2bc77a54917077b7
m-takahashi-if <18286485+mtakahashiif@users.noreply.github.com>
2021-11-10 08:36:39 +0900

    support extra root CA

--- 1.8.0/it-automation/centos8/preprocess.sh
+++ 1.8.0/it-automation/centos8/preprocess.sh
@@ -96,15 +96,15 @@ timedatectl set-timezone "${EXASTRO_ITA_SYSTEM_TIMEZONE_TABLE[$EXASTRO_ITA_LANG]
 
 
 ##############################################################################
-# install required packages
+# install common packages (installer requirements)
 
-dnf install -y rsyslog  # for writing /var/log/messages
-dnf install -y diffutils procps openssl
+dnf install -y diffutils procps which openssl
 
 
 ##############################################################################
-# install web related packages
+# install required packages
 
+dnf install -y rsyslog  # for writing /var/log/messages
 dnf install -y hostname # apache ssl needs hostname command
 
 
