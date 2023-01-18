f622f66001e3a2512d58fcf8ff4593d33b43f702
sudodaisuke-jp <66716076+sudodaisuke-jp@users.noreply.github.com>
2023-01-18 14:00:17 +0900

    Update preprocess.sh

--- 1.11.0/it-automation/ubi8/preprocess.sh
+++ 1.11.0/it-automation/ubi8/preprocess.sh
@@ -72,7 +72,7 @@ EOS
 dnf install -y dnf-plugins-core
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf config-manager --disable epel epel-modular
-
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
 
 ##############################################################################
 # Set system locale
